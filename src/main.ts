5c87107ac6b9be971d378c046ffd2feaf6b29cb1
X <ah6206123@163.com>
2024-02-28 01:01:04 +0800

    消除右边滚动条

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import {axiosGet,axiosPost,axiosDelete} from "@/serverApi/request";
 import axios from 'axios';
+import "@/assets/global.css";
 
 Vue.config.productionTip = false
 
